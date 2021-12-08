888ab10d8e447ffcd9950894d612d0f9298b4551
Andrew <boretskyiandrew@gmail.com>
2021-12-08 20:53:09 +0200

    Added .gitignore, Function Fibonacci done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <stdexcept>
 
-int main() {
-
+unsigned long long int Fibonacci(size_t n) 
+{
+if (n < 0) 
+	throw std::invalid_argument("Should not be negative!!\n");
+if (n == 0) 
+{
 	return 0;
+} if(n == 1) 
+{
+	 return 1;
+}else 
+{
+unsigned long long int result = Fibonacci(n - 1) + Fibonacci(n -2);
+return result;
+}
+}
+int main() {
+std::cout << Fibonacci(10) << "\n";
+return 0;
 }
