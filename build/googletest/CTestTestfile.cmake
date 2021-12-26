acc2e4463689e9b681b71f9e48a30971b27c349f
Andrew <boretskyiandrew@gmail.com>
2021-12-26 14:14:44 +0200

    delete googletest as folder

--- build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mdjer/progs/globallogic/recursion/googletest
-# Build directory: /home/mdjer/progs/globallogic/recursion/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
