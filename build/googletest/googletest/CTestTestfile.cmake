44a5e2c03d17481ec4773c247eb1641d7f991104
Andrew <boretskyiandrew@gmail.com>
2021-12-26 14:25:35 +0200

    Project is ready now

---
+++ build/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mdjer/progs/globallogic/recursion/googletest/googletest
+# Build directory: /home/mdjer/progs/globallogic/recursion/build/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
